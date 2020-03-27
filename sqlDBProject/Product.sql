15bbe6f1e9bbfc8697366940f511dfc296b07d0c
Amer <amermadrid7@gmail.com>
2020-03-26 23:23:11 -0300

    Add Size attribute

--- sqlDBProject/Product.sql
+++ sqlDBProject/Product.sql
@@ -3,5 +3,7 @@
 	[Id] INT NOT NULL PRIMARY KEY, 
     [Name] VARCHAR(50) NULL, 
     [Cost] INT NULL, 
-    [color] NVARCHAR(50) NULL
+    [color] NVARCHAR(50) NULL, 
+    [price] INT NULL, 
+    [size] NVARCHAR(50) NULL
 )
